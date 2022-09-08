d93c9517c8985dd8e8ffd665e41dcbf3b87e39d1
eknjustice <eknjustice@gmail.com>
2022-09-08 14:21:48 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+	printf("size of a char is %2d bytes\n", sizeof(char));
+	printf("size of an int is %2d bytes\n", sizeof(int));
+	printf("size of a long int is %2d bytes\n", sizeof(long int));
+	printf("size of a long long int is %2d bytes\n", sizeof(long long int));
+	printf("size of a float is %2d bytes\n", sizeof(float));
+	return 0;
+}
