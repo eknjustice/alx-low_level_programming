87506af6db9c407dd2251592aa1d09a80b87d672
eknjustice <eknjustice@gmail.com>
2022-09-09 15:34:03 -0700

    alphabet_3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,22 +8,22 @@
 
 int main(void)
 {
-char lower = "a";
-char upper = "A";
+	char lower = "a";
+	char upper = "A";
 
-while (lower <= "z")
-{
-putchar(lower);
-lower++;
-}
+	while (lower <= "z")
+	{
+		putchar(lower);
+		lower++;
+	}
 
-while (upper <= "Z")
-{
-putchar(upper);
-upper++;
-}
+	while (upper <= "Z")
+	{
+		putchar(upper);
+		upper++;
+	}
 
-putchar("\n")
+	putchar("\n")
 
-return (0);
+	return (0);
 }
