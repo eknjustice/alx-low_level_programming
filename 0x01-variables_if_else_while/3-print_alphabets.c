c4d82c508a2fab1b4709f29f590ed20545eeb517
eknjustice <eknjustice@gmail.com>
2022-09-09 13:25:25 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -17,7 +17,7 @@ for (alphabet = "a"; alphabet <= "z"; alphabet++)
 putchar(alphabet);
 }
 
-puchar("\n");                                                                                                                           
+putchar("\n");                                                                                                                          
 
 return (0);                                                                                                                           
 
