01c4006deb7e64ba91178a274797fc1c78a38c11
eknjustice <eknjustice@gmail.com>
2022-09-09 08:33:14 -0700

    size[

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/**
+/*
  * main - prints string in the printf function
  *
  * Description: using the main function
- * this program prints sizes of data types
+ * this program prints size "Programming is like building a multilingual puzzle
  * Return: 0
  */
 
@@ -16,10 +16,10 @@ int main(void)
 	long long int x;
 	float f;
 
-	printf("size of a char: %zu byte(s)\n", sizeof(c));
-	printf("size of an int: %zu byte(s)\n", sizeof(i));
-	printf("size of a long int: %zu byte(s)\n", sizeof(l));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(x));
-	printf("size of a float: %zu byte(s)\n", sizeof(f));
+	printf("size of a char: %d byte(s)\n", sizeof(c));
+	printf("size of an int: %d byte(s)\n", sizeof(i));
+	printf("size of a long int: %d byte(s)\n", sizeof(l));
+	printf("size of a long long int: %d byte(s)\n", sizeof(x));
+	printf("size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
