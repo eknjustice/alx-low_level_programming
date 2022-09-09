1b97b62e6bd0f0952e8d322d80be6f7e5d9ec737
eknjustice <eknjustice@gmail.com>
2022-09-08 23:40:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - print the strin in the puts function
  * description -using the main function
  * code prints Programming is like building a multilingual puzzle
- * return = 0 if the code works without errors
+ * 'main' Return: 0 if the code works without errors
  */
 
 int main(void)
