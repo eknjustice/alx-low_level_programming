652fd94f0167ba816293a353a1f9fc43ed326245
eknjustice <eknjustice@gmail.com>
2022-09-09 14:33:43 -0700

    alphabet_2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (correct)
+ */
+
+int main(void)
+{
+char alphabet;
+
+for (alphabet = "a"; alphabet ="z"; aphabet++)
+{
+putchar(alphabet);
+}
+
+putchar("\n");
+
+return (0);
+}
