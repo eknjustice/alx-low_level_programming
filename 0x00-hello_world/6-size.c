30eb39deac436d5df0803fddd0f87a1285137a68
eknjustice <eknjustice@gmail.com>
2022-09-09 08:38:03 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - prints string in the printf function
  *
  * Description: using the main function
- * this program prints size "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 
