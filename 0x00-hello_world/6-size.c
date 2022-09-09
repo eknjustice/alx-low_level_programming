88ebedda7d658ead3abaacee2bc8d70329591741
eknjustice <eknjustice@gmail.com>
2022-09-09 00:47:19 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
  * Return: 0
  */
 
-int main()
+int main(void)
 {
 	char c;
 	int i;
@@ -22,5 +22,5 @@ int main()
 	printf("size of a long long int: %zu byte(s)\n", sizeof(x));
 	printf("size of a float: %zu byte(s)\n", sizeof(f));
 	
-	return 0;
+	return (0);
 }
