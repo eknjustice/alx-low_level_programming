1ab4ba5c931b1160427b249226371a6f8e3f9f6a
eknjustice <eknjustice@gmail.com>
2022-09-09 14:17:11 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,12 +5,10 @@
  *                                                                                                                                    
  * Return: Always 0 (Correct)                                                                                                         
  */                                                                                                                                   
-                                                                                                                                      
-int main(void)                                                                                                                        
-
-{                                                                                                                                     
 char alphabet;
 
+int main(void)                                                                                                                        
+{                                                                                                                                     
 for (alphabet = "a"; alphabet <= "z"; alphabet++)
 {
 putchar(alphabet);
