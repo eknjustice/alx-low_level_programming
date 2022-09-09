e98249c7750af7696c17708d7218c5b978335429
eknjustice <eknjustice@gmail.com>
2022-09-08 23:24:05 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - prints Programming is like building a multilingual puzzle using puts function
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
