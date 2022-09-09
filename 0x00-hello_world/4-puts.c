7f0ac89e1ccc3af3bd10f64a8da925f58b4d7861
eknjustice <eknjustice@gmail.com>
2022-09-08 23:14:06 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-/**
-* put function
-* use of main function
-*/
-
 int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle")
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
 }
