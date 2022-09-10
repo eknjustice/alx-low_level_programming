f2ebc50244dfcc412d096218c430025dc842b3b3
eknjustice <eknjustice@gmail.com>
2022-09-10 02:40:27 -0700

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,20 @@
-
 #include <stdio.h>
-int main(void) {
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void) 
+{
 char c;
-for (c = 'A'; c <= 'Z'; ++c)
+c = 'a';
+while (c <= 'z')
 {   
 putchar(c);
+++c;
 putchar('\n');
 }
 return 0;
