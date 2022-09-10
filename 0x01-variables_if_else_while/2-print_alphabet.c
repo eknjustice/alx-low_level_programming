12bff6da989a6e85934f4551e146246c366d4eea
eknjustice <eknjustice@gmail.com>
2022-09-10 03:27:13 -0700

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * description - putchar prints alphabet letters 
- * Return: Always 0 (Success)
-  */
+/*
+ *main-Entry point
+ *Return-Always 0 (Success)
+ */
 int main(void)
 {
 char c;
-c = 'a';
-while (c <= 'z')
+c='a';
+while (c='z')
 {
 putchar(c);
 ++c;
