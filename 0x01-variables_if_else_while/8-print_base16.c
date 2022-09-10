a483d2aedb09af9d43f9af83c437c119535a2d9d
eknjustice <eknjustice@gmail.com>
2022-09-10 05:49:15 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - prints all numbers of base 16
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num = '0';
+char c = 'a';
+
+while (num <= '9')
+{
+putchar(num);
+num++;
+}
+while (c <= 'f')
+{
+putchar(c);
+c++;
+putchar('\n');
+
+return (0);
+}
