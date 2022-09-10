42bc6f0ceccf2def2890ce73f24c60e61a1cc461
eknjustice <eknjustice@gmail.com>
2022-09-10 05:26:54 -0700

    putchar<10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints from 0 to 10
+ * Return: Always 0 (Success0
+ */
+
+int main(void)
+{
+int number = '0';
+
+while (number <= '9')
+{
+putchar(number);
+number++;
+}
+putchar('\n');
+return (0);
+}
