7d517f9b732a61bb4fabfd0e205ae39784bba440
eknjustice <eknjustice@gmail.com>
2022-09-13 00:20:50 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main - prints the alphabet followed by a newline
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+return (0);
+}
