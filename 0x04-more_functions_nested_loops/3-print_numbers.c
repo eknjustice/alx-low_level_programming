59cb164283c763bdfdea6c135ee6aa3bacf284a0
eknjustice <eknjustice@gmail.com>
2022-09-15 18:15:26 -0700

    print_nos

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+int n;
+n = 0;
+for (n = 0; n < 10; n++)
+_putchar(n + '0');
+_putchar('\n');
+}
