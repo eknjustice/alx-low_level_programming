a298074bbb88f2d9c47c310fd0e1e66d744aa9e5
eknjustice <eknjustice@gmail.com>
2022-09-15 18:24:36 -0700

    more_nos

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints from 0 to 9 except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+if ((n != 2) && (n != 4))
+{
+_putchar('0' + n);
+}
+}
+putchar('\n');
+}
