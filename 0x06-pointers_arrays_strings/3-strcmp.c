00bb918aa467a1ea4249cbc291563473d37dd5ac
eknjustice <eknjustice@gmail.com>
2022-09-21 12:51:10 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,19 @@
 #include "main.h"
 
 /**
- * _strcmp - compare two strings
- * @s1: one string
- * @s2: one string
- * Return: int that tells num spaces in between
+ * char *_strcmp - is a function that compares
+ * @s1: first string
+ * @s2: second string 
+ * Return: the pointer to test
  */
 
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
-while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+
+while ((s1[i] == s2[i]) && ((s1[i] != '\0') || (s2[i] != '\0')))
 {
 i++;
 }
-if (*(s2 + i))
-{
-return (*(s1 + i) - *(s2 + i));
-}
-else
-{
-return (0);
-}
+return (s1[i] - s2[i]);
 }
