016a9665d777c55b7ab2d4d359ff9c10f46a5610
eknjustice <eknjustice@gmail.com>
2022-10-03 05:10:40 -0700

    static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/*
+ * _puts - prints the output to screen
+ * Return: - always 0 (success)
+ */
+
 int main(void)
 {
 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
