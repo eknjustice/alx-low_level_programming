eff16272dc68836ec0fb63a9ace41453a87086f4
eknjustice <eknjustice@gmail.com>
2022-10-04 13:18:43 -0700

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the file
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 Always (on success)
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+
+printf("%s\n", argv[0]);
+
+return (0);
+}
