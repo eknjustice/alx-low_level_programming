346bccc1e7c8332d5fad197c9192598e3f4aec14
eknjustice <eknjustice@gmail.com>
2022-10-14 08:21:11 -0700

    structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,10 @@
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @owner: Second member
+ * @age: Third member
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
